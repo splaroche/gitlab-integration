a996336d5c74b0c0224612c7877fc2dc24d1818a
GitHub Action <action@github.com>
2021-10-23 17:57:29 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/splaroche/gitlabintegration/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.splaroche.gitlabintegration.services
+
+import com.github.splaroche.gitlabintegration.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
